4b4a38a107e3a01101566dda42e4d2130566a30e
Ernesto Martin Torres Garcia <ernestomartintg@gmail.com>
2024-08-26 17:25:31 -0500

    Agregamos al build.gradle de los microservicios(product,review,recommendation,product-composite)
    jar{
    	enabled=false
    }
    para que no genere un jar plain,
    A UTIL Y API NO INGRESAR ESA CONFIGURACION

--- util/src/main/java/se/magnus/util/http/GlobalControllerExceptionHandler.java
+++ util/src/main/java/se/magnus/util/http/GlobalControllerExceptionHandler.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import se.magnus.api.exceptions.InvalidInputException;
-import se.magnus.api.exceptions.NotFoundException;
+import se.magnus.api.exceptions.*;
+
 
 import java.net.URI;
 
