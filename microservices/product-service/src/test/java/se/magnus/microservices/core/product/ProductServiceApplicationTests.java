e71139cb2c84343a05bcb1cc177e191b8cae05f0
Ernesto Martin Torres Garcia <ernestomartintg@gmail.com>
2024-08-26 21:23:35 -0500

    Test Products 2

--- microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
+++ microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
@@ -3,10 +3,13 @@ package se.magnus.microservices.core.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 import static org.springframework.http.MediaType.*;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -41,6 +44,38 @@ class ProductServiceApplicationTests {
 				.jsonPath("$.path").isEqualTo("/product/"+productNotFound)
 				.jsonPath("$.message")
 					.isEqualTo("No product found for productId: "+productNotFound);
+	}
+
+	@Test
+	void getProductInvalidParameterNegativeValue() {
+
+		int productIdInvalid = -1;
+
+		getAndVerifyProduct(productIdInvalid, UNPROCESSABLE_ENTITY)
+				.jsonPath("$.path").isEqualTo("/product/" + productIdInvalid)
+				.jsonPath("$.message").isEqualTo("Invalid productId: " + productIdInvalid);
+	}
+
+	@Test
+	void getProductInvalidParameterString() {
+
+		getAndVerifyProduct("/no-integer", BAD_REQUEST)
+				.jsonPath("$.path").isEqualTo("/product/no-integer")
+				.jsonPath("$.message").isEqualTo("Type mismatch.");
+	}
+
+	private WebTestClient.BodyContentSpec getAndVerifyProduct(int productId, HttpStatus expectedStatus) {
+		return getAndVerifyProduct("/" + productId, expectedStatus);
+	}
+
+	private WebTestClient.BodyContentSpec getAndVerifyProduct(String productIdPath, HttpStatus expectedStatus) {
+		return client.get()
+				.uri("/product" + productIdPath)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(expectedStatus)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody();
 	}
 
 }
