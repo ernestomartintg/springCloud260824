596c3888f612f99e9307b07e817c0aa62a8006d9
Ernesto Martin Torres Garcia <ernestomartintg@gmail.com>
2024-08-27 12:56:00 -0500

    Recommendation TesT ProductCompositeServiceApplicationTests Test

--- microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplicationTests.java
+++ microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -1,13 +1,60 @@
 package se.magnus.microservices.core.recommendation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class RecommendationServiceApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	void getRecommendationsByProductId(){
+		int productId=1;
+
+		client.get()
+				.uri("/recommendation?productId="+productId)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(3)
+				.jsonPath("$[0].productId").isEqualTo(productId);
 	}
 
+	@Test
+	void getRecommendationsInvalidParameter(){
+		client.get()
+				.uri("/recommendation?productId=no-integer")
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isEqualTo(BAD_REQUEST)
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.path").isEqualTo("/recommendation")
+				.jsonPath("$.message").isEqualTo("Type mismatch.");
+	}
+
+	@Test
+	void getRecommendationsNotFound(){
+		int productIdNotFound=213;
+
+		client.get()
+				.uri("/recommendation?productId="+productIdNotFound)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.length()").isEqualTo(0);
+	}
 }
