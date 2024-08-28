2bdee4b2ade4b8130a5b8d1e6f3243f65f15da5e
Ernesto Martin Torres Garcia <ernestomartintg@gmail.com>
2024-08-28 11:44:26 -0500

    Open APi 1

--- api/src/main/java/se/magnus/api/composite/ProductCompositeService.java
+++ api/src/main/java/se/magnus/api/composite/ProductCompositeService.java
@@ -2,7 +2,9 @@ package se.magnus.api.composite;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "ProductComposite", description = "REST API for composite product information.")
 public interface ProductCompositeService {
 
     @GetMapping(
