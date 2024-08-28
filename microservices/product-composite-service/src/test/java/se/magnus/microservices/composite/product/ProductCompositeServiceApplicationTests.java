5d7df3e49a4602011c81ec12af9e0621c63dd362
Ernesto Martin Torres Garcia <ernestomartintg@gmail.com>
2024-08-28 16:17:51 -0500

    Modificamos la libreria openui a la version 1.2.31

--- microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplicationTests.java
+++ microservices/product-composite-service/src/test/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplicationTests.java
@@ -40,8 +40,10 @@ class ProductCompositeServiceApplicationTests {
 	void setUp(){
 		when(compositeIntegration.getProduct(PRODUCT_ID_OK))
 				.thenReturn(new Product(PRODUCT_ID_OK,"name",1,"mock-address"));
+
 		when(compositeIntegration.getRecommendations(PRODUCT_ID_OK))
 				.thenReturn(singletonList(new Recommendation(PRODUCT_ID_OK,1,"author",1,"content","mock address")));
+
 		when(compositeIntegration.getReviews(PRODUCT_ID_OK))
 				.thenReturn(singletonList(new Review(PRODUCT_ID_OK,1,"author","subject","content","mock address")));
 
@@ -51,6 +53,13 @@ class ProductCompositeServiceApplicationTests {
 		when(compositeIntegration.getProduct(PRODUCT_ID_INVALID))
 				.thenThrow(new InvalidInputException("INVALID: "+PRODUCT_ID_INVALID));
 	}
+
+	@Test
+	void contextLoads() {
+		System.out.println(client.get().uri("/product-composite/"+PRODUCT_ID_OK).exchange());
+
+	}
+
 
 	@Test
 	void getProductById(){
