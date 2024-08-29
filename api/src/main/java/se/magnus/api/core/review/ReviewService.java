647189b0f45a673ca772f1d10b300386de0c9af5
Ernesto Martin Torres Garcia <ernestomartintg@gmail.com>
2024-08-28 22:30:34 -0500

    Review Persistence

--- api/src/main/java/se/magnus/api/core/review/ReviewService.java
+++ api/src/main/java/se/magnus/api/core/review/ReviewService.java
@@ -1,7 +1,7 @@
 package se.magnus.api.core.review;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -12,4 +12,13 @@ public interface ReviewService {
             produces = "application/json"
     )
     List<Review> getReviews(@RequestParam(value = "productId",required = true) int productId);
+
+    @PostMapping(
+            value="/review",
+            produces = "application/json"
+    )
+    Review createReview(@RequestBody Review body);
+
+    @DeleteMapping(value = "/review")
+    void deleteReviews(@RequestParam(value="productId",required = true) int productId);
 }
